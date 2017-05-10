82a3971a6c5f19c6e9cb3ac9542fbe87b394b14b
tailinwu <tyrinwu@gmail.com>
2017-05-09 23:54:58 -0400

    Add app for lists, with deliberately failing unit test

---
+++ lists/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+
+# Create your tests here.
+class SmokeTest(TestCase):
+
+    """Docstring for SmokeTest. """
+
+    def test_bad_maths(self):
+        """TODO: to be defined1. """
+        self.assertEqual(1 + 1, 3)
+
+        
