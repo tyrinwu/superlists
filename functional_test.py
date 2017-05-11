36ecbab73efaf5470c3c8bb8adfb044b5bbb43c0
tailinwu <tyrinwu@gmail.com>
2017-05-11 13:53:51 -0400

    Front page HTML now generated from a template

--- functional_test.py
+++ functional_test.py
@@ -30,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's bobby 
         # is typing fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists 
         # "1: Buy peacock features" as an item in a to-do list 
