29d3f4ca0b7020df96286f9d4331ee47092b8d5c
tailinwu <tyrinwu@gmail.com>
2017-05-12 00:27:35 -0400

    Having code smell. But still commit.

--- functional_test.py
+++ functional_test.py
@@ -40,19 +40,29 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            f"New to-do item did not appear in table. Contents were:\n{table.text}"
         )
 
         # There is still a text box inviting her to add another item. She 
         # enters "Use peacock feathers to make a fly" (Edith is very 
         # methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(2)
 
         # The page updates again, and now shows both items on her list. 
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', 
+            [row.text for row in rows]
+        )
 
         # Edith wonders whether the site will remember her list. Then she
         # sees that the site has generated a unique URL for her -- there 
         # is some explanatory etext to htat effect 
+        self.fail('Finish the test!')
 
         # Satisfied, she goes back to sleep
 
