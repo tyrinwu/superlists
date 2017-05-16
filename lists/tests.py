c8be693b5b6a8d2514bda986868833710b2d4ae4
tailinwu <tyrinwu@gmail.com>
2017-05-16 18:02:51 -0400

    Save to the database.

--- lists/tests.py
+++ lists/tests.py
@@ -2,15 +2,11 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-
 from lists.views import home_page
-
-
+from lists.models import Item
 # Create your tests here.
-class HomePageTest(TestCase):
-
-    """Docstring for HomePageTest. """
-
+class HomePageTest(TestCase):    
+    """Docstring for HomePageTest. """ 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEquals(found.func, home_page)
@@ -21,6 +17,38 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
+        
+        self.assertEqual(Item.objects.count(), 1) 
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+        
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+
+class ItemModelTest(TestCase):
+
+    """Docstring for ItemModelTest. """
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item' 
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+
+
+        
